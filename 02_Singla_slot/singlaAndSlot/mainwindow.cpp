f1915e37082d28040c3fff339c13b772b27fa143
pp <136811566@qq.com>
2018-07-11 10:38:25 +0800

    singal

--- 02_Singla_slot/singlaAndSlot/mainwindow.cpp
+++ 02_Singla_slot/singlaAndSlot/mainwindow.cpp
@@ -3,6 +3,9 @@
 #include "newpaple.h"
 #include "reader.h"
 
+//对于重载过的信号函数，需要使用定义一个函数指针指向正确的函数地址，编译器才能正确的识别出函数地址
+void (NewPaple::*newPaperNameDate)(const QString &name, const QDate &data) = &NewPaple::newsPaper;
+void (NewPaple::*newPaperName)(const QString &name) = &NewPaple::newsPaper;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,7 +16,29 @@ MainWindow::MainWindow(QWidget *parent) :
      NewPaple newspale("it's happy day!!");
      Reader reader;
      newspale.send();
-     connect(&newspale,&NewPaple::newsPaper,&reader,&Reader::receiveNewspaper);
+
+     /********************************
+     //对于糙函数，可以使用lambda表达式
+     connect(&newspale,newPaperName,[=](const Qstring &name){
+         qDebug()<<"name";
+     });
+     *********************************/
+
+
+
+
+     /********************************
+     //使用static_cast 转化函数
+
+     connect(&newspale,static_cast<void (NewPaple::*) (const QString &name)>(&NewPaple::newsPaper),
+             &reader,&Reader::receiveNewspaper);
+
+
+     *********************************/
+     connect(&newspale,newPaperNameDate,&reader,&Reader::receiveNewspaper);
+
+
+
      newspale.send();
 
 }
--- 02_Singla_slot/singlaAndSlot/newpaple.h
+++ 02_Singla_slot/singlaAndSlot/newpaple.h
@@ -3,18 +3,21 @@
 
 #include <QObject>
 
-
+class QDate;
 class QString;
+
 class NewPaple : public QObject
 {
     Q_OBJECT
 public:
     explicit NewPaple(QObject *parent = nullptr);
     explicit NewPaple(const QString &name);
+
     ~NewPaple() = default;
     void send();
 signals:
     void newsPaper(const QString &name);
+    void newsPaper(const QString &name,const QDate &date);
 public slots:
 private:
     QString m_content;
